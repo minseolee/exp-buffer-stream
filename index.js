01002898e1bb5ee43bd6eff747c71692574375ae
Minseo Lee <mmmm@ajou.ac.kr>
2024-02-21 15:54:24 +0900

    feat of downloading by buffer

--- index.js
+++ index.js
@@ -5,27 +5,48 @@ const path = require("path");
 const archiver = require("archiver");
 const { v4: uuidV4 } = require('uuid');
 const Monitor = require('./monitor');
+const compressing = require("compressing");
 
 
 const app = express();
 
 const FILE_PATH = path.join(__dirname, './json_files');
 const TEMP_PATH = path.join(__dirname, './temp');
-const ZIP_PATH = '';
 
 
 app.get('/experiment/buffer', async (req, res) => {
    try {
+      const zip = (source, destination) => {
+         return compressing.zip.compressDir(source, destination);
+      };
 
+      const monitor = new Monitor(process, performance, 100);
+      const startTime = monitor.getPerfTime();
+      monitor.startMemoryMonitor();
+
+      const files = await fs.promises.readdir(FILE_PATH)
+      const uuid = uuidV4();
+
+      const ZIP_PATH = path.join(__dirname, `/${uuid}.zip`);
+
+      for (const file of files)
+         await fs.promises.copyFile(path.join(__dirname, '/json_files', file), path.join(TEMP_PATH, file));
+
+
+      await zip(TEMP_PATH, ZIP_PATH);
+
+
+      const endTime = performance.now();
+      monitor.clearMemoryMonitor();
+      monitor.log(startTime, endTime)
+
+      await fs.promises.rm(path.join(__dirname, `/${uuid}.zip`));
+
+      return res.status(200).send({ uuid });
    } catch (e) {
       console.error(e);
       return res.status(500).send({ message: '500' });
    }
-
-   await fs.promises.copyFile(FILE_PATH, TEMP_PATH);
-
-
-   await res.download(FILE_PATH);
 });
 
 
