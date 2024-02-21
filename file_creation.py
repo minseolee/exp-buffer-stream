a844b851e5681684cb04f9b1d7b1f6cc71118181
Minseo Lee <mmmm@ajou.ac.kr>
2024-02-21 13:39:05 +0900

    file upload

---
+++ file_creation.py
@@ -0,0 +1,26 @@
+import json
+import os
+import random
+import string
+
+def generate_random_string(length):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+def generate_json_file(file_path, size_in_bytes):
+    data = {
+        "random_data": generate_random_string(size_in_bytes)
+    }
+    with open(file_path, 'w') as file:
+        json.dump(data, file)
+
+if __name__ == "__main__":
+    total_files = 100
+    target_file_size_mb = 5
+    target_file_size_bytes = target_file_size_mb * 1024 * 1024  # MB to bytes
+
+    for i in range(1, total_files + 1):
+        file_name = f"data_{i}.json"
+        file_path = os.path.join("json_files", file_name)
+        generate_json_file(file_path, target_file_size_bytes)
+        print(f"Generated: {file_path}")
+
---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const fs = require("fs");
+const zlib = require("zlib");
+const path = require("path");
+const archiver = require("archiver");
+const { v4: uuidV4 } = require('uuid');
+
+
+const app = express();
+
+const FILE_PATH = path.join(__dirname, './json_files');
+const TEMP_PATH = path.join(__dirname, './temp');
+const ZIP_PATH = '';
+
+
+app.get('/experiment/buffer', async (req, res) => {
+   await fs.promises.copyFile(FILE_PATH, TEMP_PATH);
+
+
+   await res.download(FILE_PATH);
+});
+
+
+app.get('/experiment/pipe', async (req, res) => {
+   try {
+      let MAX_rss = 0;
+      let MAX_heapTotal = 0;
+      let MAX_heapUsed = 0;
+      let MAX_external = 0;
+
+      let SUM_rss = 0;
+      let SUM_heapTotal = 0;
+      let SUM_heapUsed = 0;
+      let SUM_external = 0;
+
+      let iteration = 0;
+
+      const memoryMonitor = setInterval(() => {
+         const { rss, external, heapTotal, heapUsed } = process.memoryUsage();
+         if (MAX_rss < rss) MAX_rss = rss;
+         if (MAX_external < external) MAX_external = external;
+         if (MAX_heapUsed < heapUsed) MAX_heapUsed = heapUsed;
+         if (MAX_heapTotal < heapTotal) MAX_heapTotal = heapTotal;
+
+         SUM_rss += rss;
+         SUM_external += external;
+         SUM_heapUsed += heapUsed;
+         SUM_heapTotal += heapTotal;
+
+         iteration++;
+
+         console.log(rss, external, heapTotal, heapUsed, iteration);
+      }, 1);
+
+
+      fs.readdir(FILE_PATH, (err, files) => {
+         if (err) return;
+
+         const uuid = uuidV4();
+         const output = fs.createWriteStream(__dirname + `/${uuid}.zip`);
+         const archive = archiver('zip', {zlib: {level: 9}});
+
+         archive.on('error', (err) => { throw err; });
+
+         archive.pipe(output);
+
+         for (const file of files)
+            archive.append(fs.createReadStream(path.join(FILE_PATH, file)), {name: file});
+
+         archive.finalize();
+
+         clearInterval(memoryMonitor);
+         console.log('total iter', iteration);
+         console.log('max', MAX_rss, MAX_external, MAX_heapUsed, MAX_heapTotal);
+         console.log('avg', SUM_rss / iteration, SUM_external / iteration, SUM_heapUsed / iteration, SUM_heapTotal / iteration);
+
+         return res.status(200).send({ uuid });
+      })
+   } catch (e) {
+      console.error(e);
+   }
+});
+
+
+app.listen(5050, async () => {
+   console.log('server on 5050');
+})
