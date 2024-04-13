7aba9db3f49d31f8e69bfcb9aa92487035b04c96
Minseo Lee <mmmm@ajou.ac.kr>
2024-04-13 20:35:28 +0900

    new plot

--- plot.py
+++ plot.py
@@ -4,11 +4,12 @@ import numpy as np
 
 names = ['quantity', 'eachSize', 'fileType', 'totalSize', 'eachSize2', 'totalSize2', 'runTime', 'totalIter', 'minCPU', 'maxCPU', 'avgCPU', 'minMEM', 'maxMEM', 'avgMEM']
 x = 'eachSize'
-y = 'avgCPU'
+y = 'runTime'
+type = 'json'
 usecols = [x, y]
 
-copy_path = 'exp-results/refine/copy_size_json.csv'
-stream_path = 'exp-results/refine/stream_size_json.csv'
+copy_path = f'exp-results/refine/copy_size_{type}.csv'
+stream_path = f'exp-results/refine/stream_size_{type}.csv'
 
 log2 = True
 
@@ -24,12 +25,12 @@ stream_mean = stream_data.groupby(x)[y].mean().reset_index()
 # 그래프 그리기
 fig, ax = plt.subplots(figsize=(6, 8))
 
-ax.plot(np.log2(copy_mean[x]), copy_mean[y], marker='o', label='copy')
-ax.plot(np.log2(copy_mean[x]), stream_mean[y], marker='s', label='stream')
+ax.plot(copy_mean[x], copy_mean[y], marker='o', label='copy')
+ax.plot(copy_mean[x], stream_mean[y], marker='s', label='stream')
 
-ax.set_xlabel('Size log2 increase .json')
-ax.set_ylabel('CPU usage')
-ax.set_title('Average CPU Usage Comparison')
+ax.set_xlabel(f'Size increase .{type}')
+ax.set_ylabel(y)
+ax.set_title('Average Runtime Comparison')
 ax.legend()
 
 plt.show()
